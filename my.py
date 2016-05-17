1d7cbcddc214f2b0b130a19c67d197f4af6bbf96
amonelk <yangke.yk@alibaba-inc.com>
2016-05-17 14:16:20 +0800

    123

--- my.py
+++ my.py
@@ -3,11 +3,11 @@ from flask import render_template
 
 app = Flask(__name__)
 app.config.update(
-    SERVER_NAME='45.78.36.201:80'
+    SERVER_NAME='127.0.0.1:8080'
 )
 @app.route('/<name>')
 def hello_world(name):
     # return 'Hello World!'
     return render_template('hello.html', name=name, user='ooooop!')
 if __name__ == '__main__':
-    app.run()
+    app.run('0.0.0.0')
