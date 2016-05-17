c6c3f32608c78b561a3390e07f6c4a51c3a5a5a2
amonelk <yangke.yk@alibaba-inc.com>
2016-05-17 14:38:17 +0800

    123

--- my.py
+++ my.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
-@app.route('/')
-def hello_world():
+@app.route('/<name>')
+def hello_world(name):
     # return 'Hello World!'
-    return render_template('hello.html', name='name', user='ooooop!')
+    return render_template('hello.html', name=name, user='ooooop!')
 if __name__ == '__main__':
     app.run('0.0.0.0')
